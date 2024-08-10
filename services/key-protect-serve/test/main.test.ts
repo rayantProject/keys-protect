45d2fad26ace6f1f17a550836c9141ca60609d11
TRAORE RAYAN <rayan.traore03@gmail.com>
2024-08-11 00:28:50 +0200

    feat: add desktop kit with nextron

--- services/key-protect-serve/test/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { helloWorld } from './../src/main';
-
-describe('Hello world', () => {
-    test('should return hello world', () => {
-        expect(helloWorld()).toBe('Hello World');
-    });
-});
